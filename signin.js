774f79f2c2dec72df26ac629ef8e4a726b85a58b
FranM28 <Fran_moore8@hotmail.com>
2015-03-12 16:48:31 +0000

    FM - Updated signin.js to have the full login flow
    Previously not working as IDP not selected at the start

--- signin.js
+++ signin.js
@@ -1,36 +1,59 @@
-// Currently work in progress
-
-casper.test.begin('Sign In User', 1, function(test){
-    casper.start('https://dyfi.cobwebproject.eu/idp/Authn/UserPassword');
-
+// Test script to perform a user login on the portal, produces screen captures to show before and after
+casper.test.begin('Test the log in function', 3, function suite(test) {
+    casper.start('https://dyfi.cobwebproject.eu', function() {
 	
-		casper.waitUntilVisible('form#login', function() {
+		// First tests that the url to search is available before clicking it
+		test.assertExists('i.fa.glyphicon.glyphicon-log-in');
+    });
+	
+	// ensures the link is available to click
+	casper.waitUntilVisible('i.fa.glyphicon.glyphicon-log-in', function() {
+        this.click('i.fa.glyphicon.glyphicon-log-in');
+		
+    });
+	
+	casper.wait(3000, function(){
+	console.log('clicked ok, new location is ' + this.getCurrentUrl());
+	});
+	
+	casper.waitUntilVisible('button.btn.btn-success', function() {
+        test.assertExists('button.btn.btn-success');
+		this.click('button.btn.btn-success');
+    });
+	
+	casper.wait(3000, function(){
+	console.log('cobweb idp clicked ok, new location is ' + this.getCurrentUrl());
+	});
+	
+	casper.waitUntilVisible('form#login', function() {
 		
         this.fill('form#login', {
-					'j_username' : 'franm28',
-					'j_password' : ''
+					'j_username' : '', // enter your username here
+					'j_password' : '' // enter your password here
 
 				}, true);
     });
+	
+	// ensures the link is available to click, for some reason the link has to be clicked twice to recognise the fact you are logged in
+	casper.waitUntilVisible('i.fa.glyphicon.glyphicon-log-in', function() {
+        this.click('i.fa.glyphicon.glyphicon-log-in');
 		
-		//this.click('button.form-element.form-button');
-   
-	
-	 casper.wait(10000, function(){
-		test.assertHttpStatus(200, 'this status says it is ok, even though your password is wrong');
-       this.echo(this.getHTML());
-    });
-
-   
-
-	casper.wait(10000, function(){
-	this.echo(this.getHTML());
-      
     });
 	
-	 phantom.clearCookies();
-
-    casper.run(function(){
+	// Set the screen size
+	casper.then(function(){
+		this.viewport(1920, 1200);
+	});
+	
+	casper.wait(5000, function(){
+	  test.assertExists('i.fa.glyphicon.glyphicon-log-out', "log out link visible, user is logged in");
+     casper.capture("return.png"); 
+    });
+	
+	// Run all tests and finish
+	casper.run(function() {
         test.done();
     });
+	
+	phantom.clearCookies();
 });
