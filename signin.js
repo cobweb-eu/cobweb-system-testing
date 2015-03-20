31b8a46098a25f045d303cef17e160ebfa8fd159
Sebastian Clarke <sebastian.clarke@envsys.co.uk>
2015-03-20 15:27:44 +0000

    Big refactor changing signing to xpath and rationalising wait code. More reliable and better xml output now

--- signin.js
+++ signin.js
@@ -1,129 +1,121 @@
 // Test script to perform a user login on the portal, produces screen captures to show before and after
-var links;
-var idpLinks;
-var loginButton;
-var secondLink;
-var loggedInLinks;
-var logOutButton;
+// Re-factored to use xpath to locate elements and to use waitFor to speed up execution.
+// Simpler tests using xpath should mean better reporting and more reliable operation
 
-casper.test.begin('Test the log in function', 4, function suite(test) {
+var firstUrl;
+var x = require('casper').selectXPath;
 
-	casper.start('https://dyfi.cobwebproject.eu', function() {
-		// Retrieves a list of menu options available in the top bar
-		links = this.evaluate(function() {
-            var elements = __utils__.findAll('i');
-            return elements.map(function(e) {
-                return e.getAttribute('class');
-            });
-        });
+var XPATH_LOGIN_LINK = '//a[text()=" Login"]';
+var XPATH_LOGOUT_LINK = '//a[text()=" Logout"]';
+var XPATH_COBWEB_IDP_BUTTON = '//button[1]';
+var XPATH_COOKIE_ACCEPT = '//*[@id="ng-app"]/body/div[1]/div[4]/div/div[1]/p[3]/span[1]';
+
+casper.test.begin('Test login functionality', 8, function suite(test) {
+	casper.start('https://dyfi.cobwebproject.eu');
+	
+    casper.then(function () {
+        this.viewport(1920, 1200);
     });
-
-	
+    
+    // this may help mitigate no logout visible bug
+	casper.thenClick(x(XPATH_COOKIE_ACCEPT), function() {
+	    console.log("Accepting cookie policy");
+	});
+	    	
 	casper.then(function() {
-		console.log('looking for login button');
-		// This is the button to click to log in within the set of controls returned
-		secondLink = links[2];
-		// Start building up the control name to use in the button click event
-		secondLink = secondLink.replace(/ /g, ".");
-		secondLink = 'i.' + secondLink;
-		// Check the button exists
-		this.test.assertExists(secondLink, "login link is found");
-		// Click the button
-		this.click(secondLink);
+		this.test.assertVisible({
+		    type: 'xpath',
+			path: XPATH_LOGIN_LINK
+		}, 'Login link found');
+		
+		this.test.assertNotVisible({
+		    type: 'xpath',
+			path: XPATH_LOGOUT_LINK
+		}, 'No logout link');
+   
+		firstUrl = this.getCurrentUrl();
 	});
+		    
+	casper.thenClick(x(XPATH_LOGIN_LINK), function() {
+        console.log("Clicked login link");
+    });
+    
+    casper.waitFor(
+        function check() {
+            return this.getCurrentUrl() != firstUrl;
+        }, function then() {
+            casper.test.pass('WAYF Page Loaded');
+        }, function timeout() {
+            casper.test.fail('Timed out waiting for WAYF page to load (5s)', {name: 'WAYF Page Loaded'});
+        }
+    );
+		
+    casper.then(function() {
+		this.test.assertExists({
+		    type: 'xpath',
+			path: XPATH_COBWEB_IDP_BUTTON
+		}, 'COBWEB IDP button found');
+		firstUrl = this.getCurrentUrl();
+    });
+    
+	casper.thenClick(x(XPATH_COBWEB_IDP_BUTTON), function() {
+        console.log("Clicked COBWEB IDP button");
+    });
+    
+    casper.waitFor(
+        function check() {
+            return this.getCurrentUrl() != firstUrl;
+        }, function then() {
+            casper.test.pass("COBWEB Login page loaded");
+        }, function timeout() {
+            caspter.test.fail("Timed out waiting for COBWEB login page to load (5s)", {name: 'COBWEB Login page loaded'});
+        }
+    );
 
-	
-	casper.wait(3000, function(){
-		// Checks the url of the page once the login button is clicked
-		console.log('clicked login ok, new location is ' + this.getCurrentUrl());
-		// Function to retrieve all the button controls on the new page
-		idpLinks = this.evaluate(function() {
-			var elements = __utils__.findAll('button');
-			return elements.map(function(e) {
-				return e.getAttribute('class');
-			});
-		});
-	});
-
-	
-	casper.then(function(){
-		// Gets the login with Cobweb account button (1st on page)
-		loginButton = idpLinks[0];
-		// Builds up the control name to use in the button click event
-		loginButton = loginButton.replace(/ /g, ".");
-		loginButton = 'button.' + loginButton;
-		// Tests the button exists
-		test.assertExists(loginButton, "login with cobweb button found");
-		// Clicks the button
-		this.click(loginButton);
-	});
-
-
-	casper.wait(3000, function(){
-		// Gets the URL of the new page after button click
-		console.log('cobweb idp clicked ok, new location is ' + this.getCurrentUrl());
-	});
-
-	
-	casper.wait(3000, function() {
+	casper.then(function() {
 		// Complete the login form
-		console.log('filling in form');
+		console.log('Posting to login form');
+		firstUrl = this.getCurrentUrl();
         this.fill('form#login', {
-		    'j_username' : '', // enter your username here
-		    'j_password' : '' // enter your password here
+		    'j_username' : 'sebclarke', // enter your username here
+		    'j_password' : 'password' // enter your password here
 		}, true);
     });
-
-	
-    /* 
-    The following block finds and clicks the login link again.
-    This is necessary to mitigate a bug with the portal that
-    this login link needs to be clicked again, after login, to
-    update the portal page/context with logged in info.
-
-    As this is a BUG, rather than a feature, I am removing this
-    from the test as we should test for correct operation. 
-    Hopefully this will raise visibility and get the bug fixed!
-    */
     
-    
-	/*casper.wait(3000, function() {
-		console.log('trying to click the link again');
-		test.assertExists(secondLink);
-		this.click(secondLink);	
+    casper.waitFor(
+        function check() {
+            return this.getCurrentUrl() != firstUrl;
+        }, function then() {
+            casper.test.pass("Login form returned");
+        }, function timeout() {
+            caspter.test.fail("Timed out waiting for login form to return(5s)", {name: 'Login form returned'});
+        }
+    );
+   
+    // try click the login link again because of bug!
+    /*   
+    casper.thenClick(x(XPATH_LOGIN_LINK), function() {
+        console.log("Clicked login link again");
     });*/
-	
-
-	// Set the screen size
-	casper.then(function(){
-		this.viewport(1920, 1200);
-	});
-
-	
-	casper.wait(5000, function(){
-		// Retrieves the available menu options in the top navigation bar
-	    loggedInLinks = this.evaluate(function() {
-            var elements = __utils__.findAll('i');
-            return elements.map(function(e) {
-                return e.getAttribute('class');
-            });
-        });
-	});
-
-	
-	casper.then(function(){
-		// Retrieves the log out button details from the array of links
-		logOutButton = loggedInLinks[3];
-		// Build up the control name to be used in the button click event
-		var newLogOutBtn = logOutButton.replace(/ /g, ".");
-		logOutButton = 'i.' + newLogOutBtn;
-		// Check the button exists
-		test.assertExists(logOutButton, "User logged in: logout button found");
-		// Return a screenshot to check and confirm that the login link is no longer visible
-		casper.capture("return.png"); 
-	});
-	
-
+    
+    casper.then(function() {
+        console.log('Looking for logout link');
+		this.test.assertVisible({
+		    type: 'xpath',
+			path: XPATH_LOGOUT_LINK
+		}, 'logout link found');
+		
+		this.test.assertNotVisible({
+		    type: 'xpath',
+			path: XPATH_LOGIN_LINK
+		}, 'Login link found');
+    });
+    
+    
+    casper.then(function() {
+        casper.capture("./logged_in_new.png");
+    });
+    
 	// Run all tests and finish
 	casper.run(function() {
         test.done();
