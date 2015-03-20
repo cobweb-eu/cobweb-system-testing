278b65a3829fd6f399e097c86e59441e918d5e5b
Sebastian Clarke <sebastian.clarke@envsys.co.uk>
2015-03-20 15:54:21 +0000

    only run tests starting with cobweb-*

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for testfile in `ls *.js`; do
+for testfile in `ls cobweb-*.js`; do
 	casperjs test --ssl-protocol=tlsv1 --xunit=$testfile.xml --log-level=debug $testfile --proxy-type=none;
 done;
