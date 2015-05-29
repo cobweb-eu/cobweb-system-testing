d1c3351aff0217bf60603f5ff07c0a6103c08651
Sebastian Clarke <sebastian.clarke@envsys.co.uk>
2015-05-29 11:17:39 +0100

    (re)moving casper tests

--- absorb_results.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python /usr/local/src/envsys/django/cobweb/cobwebtesting/tools/xunit_result_importer.py -x signin.js.xml -c;
--- run_all_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for testfile in `ls cobweb-*.js`; do
-	casperjs test --ssl-protocol=tlsv1 --xunit=$testfile.xml --log-level=debug $testfile --proxy-type=none;
-done;
