c23c51b84eb970dbdef7c2d25ae6e164f4f52adc
Mark Higgins <mark.higgins@envsys.co.uk>
2015-06-04 16:18:23 +0100

    Input the name of the public survey into the search field.

--- selenium/cobweb_publishing_tests.py
+++ selenium/cobweb_publishing_tests.py
@@ -46,7 +46,8 @@ class PublishTests(COBWEBSurveyTest):
             EC.visibility_of_element_located((By.XPATH, NAVBAR_SEARCH))
         ).click()
         #Input the title of Public Survey, press search/enter
-        
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="gn-any-field"]'))
+        ).send_keys(%public_survey_name)
         #If survey is visible, true, if not, false/fail
         
         #Next, test for visibility of the Private Survey. Return to homepage, sign-in as Registered User account
