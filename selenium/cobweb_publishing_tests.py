70659c84a21c90c45e10c3afcb100e02a1bbb2ce
Mark Higgins <mark.higgins@envsys.co.uk>
2015-06-11 16:59:46 +0100

    Changed to Firefox due to bug. Added wait for xpath to allow metadata editor to fully load. Changed error message to suit.

--- selenium/cobweb_publishing_tests.py
+++ selenium/cobweb_publishing_tests.py
@@ -13,7 +13,8 @@ from cobweb_reg_use_case_tests import COBWEBSurveyTest
 class PublishTests(COBWEBSurveyTest):
     
     def setUp(self):
-        self.driver = webdriver.Chrome('C:\Users\Admin.ENVSYS-704\Desktop\Extra\chromedriver_win32\chromedriver.exe')
+        """self.driver = webdriver.Chrome('C:\Users\Admin.ENVSYS-704\Desktop\Extra\chromedriver_win32\chromedriver.exe')"""
+        self.driver = webdriver.Firefox()
         self.driver.set_window_size(1280, 1024)
         self.wait = WebDriverWait(self.driver, 20)
         self.long_wait = WebDriverWait(self.driver, 50)
@@ -21,21 +22,20 @@ class PublishTests(COBWEBSurveyTest):
         self.PASSWORD = 'webboc_9625'
         self._accept_cookie_sign_in()
         self.public_survey_name = self._create_survey("PubTest Public")
-        
         self.driver.get(CS.PRIV_URL)
         self.private_survey_name = self._create_survey("PubTest Private")
         survey_link = self.get_by_xpath('//a[text()="%s"]'%self.public_survey_name)
         edit_link = survey_link.find_element_by_xpath(CS.XPATH_SURVEY_EDIT_REL)
         edit_link.click()
         self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, CS.METADATA_BOUNDBOX))
+        )
+        self.wait.until(
             EC.visibility_of_element_located((By.XPATH, CS.PUBLISH_BUTTON))
-        
         ).click()
-        
         self.wait.until(
             EC.visibility_of_element_located((By.XPATH, CS.UNPUBLISH_BUTTON))
         )
-        
         self.get_by_xpath(CS.METADATA_SAVECLOSE_BUTTON).click()
         self.wait.until(
             EC.visibility_of_element_located((By.XPATH, CS.SURVEY_LIST_AREA))
@@ -47,9 +47,7 @@ class PublishTests(COBWEBSurveyTest):
         self.driver.close()
         
     def test_visible(self):
-        
         # Delete cookies to clear login, return to the homepage
-        self.driver.delete_all_cookies() 
         self.driver.get(CS.LIVE_URL)
         # Access Search Tool
         self.wait.until(
@@ -60,12 +58,10 @@ class PublishTests(COBWEBSurveyTest):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, CS.NSEARCH_INPUT))
         ).send_keys(self.public_survey_name)
         self.get_by_xpath(CS.NSEARCH_SUBMIT).click()
-        
         # If survey is visible, true, if not, false/fail
         self.wait.until(
             EC.visibility_of_element_located((By.XPATH, '//a[contains(., "%s")]'%self.public_survey_name))
         )
-        
         # Check private survey is not shown!
         self.wait.until(
             EC.visibility_of_element_located((By.XPATH, CS.NSEARCH_CLEAR))
@@ -78,7 +74,9 @@ class PublishTests(COBWEBSurveyTest):
         
         # Next, test for visibility of the Private Survey. Return to homepage, sign-in as Registered User account
         self.driver.get(CS.PRIV_URL)
-        
+        self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, CS.LOGOUT_LINK))
+        ).click()
         self.USERNAME = "PublishTester1"
         self.PASSWORD = "password"
         self._login_with(self.USERNAME, self.PASSWORD)
@@ -86,7 +84,7 @@ class PublishTests(COBWEBSurveyTest):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, CS.NSEARCH_INPUT))
         ).send_keys(self.private_survey_name)
         self.get_by_xpath(CS.NSEARCH_SUBMIT).click()
-        self.assertEqual(len(matching_survey_links), 1, 'Private survey visible when not logged in!')
+        self.assertEqual(len(matching_survey_links), 1, 'Private survey NOT visible when logged in!')
         
         # If survey is visible, true, if not false/fail. Portal end of test is complete.
         self.assertTrue(False)
